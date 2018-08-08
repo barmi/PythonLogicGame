96604e29c81961c1c3b0155e551293df0e95317d
skshin <skshin@gmail.com>
2018-08-08 14:56:16 +0900

    src/week02/guess.py: 시도횟수를 마지막에 보여주는 기능 추가

--- src/week02/guess.py
+++ src/week02/guess.py
@@ -2,14 +2,16 @@ import random
 
 correct = False
 com_num = random.randint(1,100)
+try_count = 1
 
 while correct == False:
     you_num = int(input("숫자를 입력하세요(1~100) : "))
     if you_num == com_num:
         correct = True
-        print("정답입니다.")
+        print("정답입니다.", try_count, "번만에 맞췄습니다.")
     else:
         if you_num > com_num:
             print("큽니다.");
         else:
             print("작습니다.");
+    try_count += 1
