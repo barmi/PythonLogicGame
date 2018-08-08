1f2d48b52ede2724d7d8c77292f730f8026d5664
skshin <skshin@gmail.com>
2018-08-08 14:53:17 +0900

    src/week02/guess.py: 입력한 숫자의 크다,작다를 표시

--- src/week02/guess.py
+++ src/week02/guess.py
@@ -4,7 +4,12 @@ correct = False
 com_num = random.randint(1,100)
 
 while correct == False:
-    you_num = input("숫자를 입력하세요(1~100) : ")
+    you_num = int(input("숫자를 입력하세요(1~100) : "))
     if you_num == com_num:
-        corrrect = True
+        correct = True
         print("정답입니다.")
+    else:
+        if you_num > com_num:
+            print("큽니다.");
+        else:
+            print("작습니다.");
