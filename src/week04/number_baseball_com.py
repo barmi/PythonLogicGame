1ce6078c447d33091ecee9862b73be6f79621bc9
skshin <skshin@gmail.com>
2018-08-10 17:46:21 +0900

    src/week04/number_baseball_com.py: 사람의 볼카운트를 입력받아서 테이블을 갱신하는 함수 추가

--- src/week04/number_baseball_com.py
+++ src/week04/number_baseball_com.py
@@ -24,6 +24,9 @@ def is_valid_num(num):
 def is_valid_numstr(numstr):
     return ((numstr[0] != numstr[1]) and (numstr[0] != numstr[2]) and (numstr[1] != numstr[2]))
 
+def input_ball_count():
+    return [int(x) for x in input("볼카운트를 입력하세요(스트라크 볼) ").split()]
+
 # ==============================================================================
 # check table 관련함수
 def init_check_table():
@@ -38,8 +41,24 @@ def find_valid_num_in_table(table):
         num = random.randint(1, 999)
         if table[num]:
             return num
+
+def update_check_table(table, check_numstr, strike, ball):
+    for num in range(0, 1000):
+        if table[num]:
+            numstr = str(num).zfill(3)
+            s,b = get_ball_count(numstr, check_numstr)
+            if s != strike or b != ball:
+                table[num] = False
 # ==============================================================================
 
 check_table = init_check_table()
 print(check_table.count(True))
-print(find_valid_num_in_table(check_table))
+guess_num = find_valid_num_in_table(check_table)
+guess_numstr = str(guess_num).zfill(3)
+print(guess_num)
+s, b = input_ball_count()
+update_check_table(check_table, guess_numstr, s, b)
+print(check_table.count(True))
+for num in range(0, 1000):
+    if check_table[num]:
+        print(str(num).zfill(3))
